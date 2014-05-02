8f5a9002b2045219995e83691ea1e62599913dc7
ShiZhan <g.shizhan.g@gmail.com>
2014-05-01 21:32:08 -0500

    m * n

--- src/main/scala/MazeGenerator.scala
+++ src/main/scala/MazeGenerator.scala
@@ -1,9 +1,9 @@
-case class Maze(size: Int) {
+case class Maze(height: Int, width: Int) {
   import scala.util.Random
   private def wall = if (Random.nextInt(10) > 3) 1 else 0
-  private val cells = Iterator.tabulate(size + 1) { r => (
-    Iterator.tabulate(size + 1) { v => if (r == 0) 1 else if (v == 0 || v == size) 0 else wall },
-    Iterator.tabulate(size + 1) { h => if (h == 0) 1 else if (r == 0 || r == size) 0 else wall })
+  private val cells = Iterator.tabulate(height + 1) { r => (
+    Iterator.tabulate(width + 1) { v => if (r == 0) 1 else if (v == 0 || v == width) 0 else wall },
+    Iterator.tabulate(width + 1) { h => if (h == 0) 1 else if (r == 0 || r == height) 0 else wall })
   }
 
   override def toString =
@@ -15,7 +15,7 @@ case class Maze(size: Int) {
 
 object MazeGenerator {
   def main(args: Array[String]) = {
-    val m = Maze(args.head.toInt)
+    val m = Maze(args(0).toInt, args(1).toInt)
     println(m)
   }
 }
