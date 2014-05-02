4610058632767215dcc74d1f284d8486258809c6
ShiZhan <g.shizhan.g@gmail.com>
2014-05-02 04:08:18 -0500

    reachability

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,5 +1,22 @@
 case class Graph(m: Array[Array[Int]]) {
+  val vertexTotal = m.length
+
+  assert(vertexTotal == m.head.length)
+
   override def toString = m.map(_.mkString(" ")).mkString("\n")
+
+  private def _reachable(from: Int, to: Int, visited: Array[Boolean]): Boolean = {
+    visited(from) = true
+    if (from == to) true
+    else
+      m(from).zipWithIndex.exists {
+        case (edge, vertex) =>
+          !visited(vertex) && edge > 0 && _reachable(vertex, to, visited)
+      }
+  }
+
+  def reachable(from: Int, to: Int) =
+    _reachable(from, to, Array.fill(vertexTotal)(false))
 }
 
 object GraphReachability {
@@ -9,7 +26,7 @@ object GraphReachability {
     val vertexTotal = args(0).toInt
     val m = Array.fill(vertexTotal)(Array.fill(vertexTotal)(Random.nextInt(2)))
     val g = Graph(m)
-
     println(g)
+    println(g.reachable(0, vertexTotal - 1))
   }
 }
