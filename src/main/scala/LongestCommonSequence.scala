d29bf1622568e2f590e20482f916f0669fb65686
ShiZhan <g.shizhan.g@gmail.com>
2014-05-04 04:40:32 -0500

    clean

--- src/main/scala/LongestCommonSequence.scala
+++ src/main/scala/LongestCommonSequence.scala
@@ -1,13 +1,13 @@
 object LongestCommonSequence {
   implicit class LCSop[T](left: Seq[T]) {
     private def get(t: Array[Array[Int]], i: Int, j: Int) = if (i < 0 || j < 0) 0 else t(i)(j)
+
     def <=>(right: Seq[T]) = {
       val t = Array.fill(left.length)(Array.fill(right.length)(0))
       for ((l, i) <- left.zipWithIndex)
-        for ((r, j) <- right.zipWithIndex) {
+        for ((r, j) <- right.zipWithIndex)
           t(i)(j) = if (l == r) get(t, i - 1, j - 1) + 1 else get(t, i - 1, j) max get(t, i, j - 1)
-        }
-      //t.foreach(line => println(line.mkString(" ")))
+
       val lcsLen = t(left.length - 1)(right.length - 1)
       val lcs = Array.fill(lcsLen)((0, 0))
       var (i, j) = (left.length - 1, right.length - 1)
