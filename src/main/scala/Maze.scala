834c8b52adf1b6d56d9df71ea9d9d921b5203ecb
ShiZhan <g.shizhan.g@gmail.com>
2014-05-11 17:20:52 -0500

    reorganize code

--- src/main/scala/Maze.scala
+++ src/main/scala/Maze.scala
@@ -1,42 +1,53 @@
-case class SimpleMaze(height: Int, width: Int) {
-  import scala.util.Random
+class Maze(height: Int, width: Int) {
+  val cells = Array.fill(height + 1, width + 1)((0, 0))
 
-  private def wall = if (Random.nextInt(10) > 3) 1 else 0
-  private val cells = Array.tabulate(height + 1) { r =>
-    Array.tabulate(width + 1) { c =>
-      val v = if (r == 0) 1 else if (c == 0 || c == width) 0 else wall
-      val h = if (c == 0) 1 else if (r == 0 || r == height) 0 else wall
-      (v, h)
-    }
-  }
-
-  override def toString = cells.map {
-    case row =>
-      val (v, h) = row.unzip
-      val vLine = v.map { case 0 => "|"; case _ => " " }.mkString("  ", " ", "\n")
-      val hLine = h.map { case 0 => "-"; case _ => " " }.mkString(" ", "+", "+\n")
-      vLine + hLine
+  override def toString = cells.map { row =>
+    val (v, h) = row.unzip
+    val vLine = v.map { case 1 => "|"; case _ => " " }.mkString("  ", " ", "\n")
+    val hLine = h.map { case 1 => "-"; case _ => " " }.mkString(" ", "+", "+\n")
+    vLine + hLine
   }.mkString
 
   def toGraph = {
     val cellTotal = height * width
     val g = Array.fill(cellTotal, cellTotal)(0)
-
     for (i <- (0 to (height - 1)))
       for (j <- (0 to (width - 1))) {
         val (v, h) = cells(i + 1)(j + 1)
         val thisCell = i * width + j
         val southCell = (i + 1) % height * width + j
         val eastCell = i * width + ((j + 1) % width)
-        if (v == 1) { g(thisCell)(eastCell) = 1; g(eastCell)(thisCell) = 1 }
-        if (h == 1) { g(thisCell)(southCell) = 1; g(southCell)(thisCell) = 1 }
+        if (v == 0) { g(thisCell)(eastCell) = 1; g(eastCell)(thisCell) = 1 }
+        if (h == 0) { g(thisCell)(southCell) = 1; g(southCell)(thisCell) = 1 }
       }
     g
   }
 }
 
-case class ConnectedMaze(height: Int, width: Int) {
-  
+case class SimpleMaze(height: Int, width: Int) extends Maze(height, width) {
+  import scala.util.Random
+
+  private def wall = if (Random.nextInt(10) > 3) 1 else 0
+
+  (0 to height).foreach { r =>
+    (0 to width).foreach { c =>
+      val v = if (r == 0) 0 else if (c == 0 || c == width) 1 else wall
+      val h = if (c == 0) 0 else if (r == 0 || r == height) 1 else wall
+      cells(r)(c) = (v, h)
+    }
+  }
+}
+
+case class ConnectedMaze(height: Int, width: Int) extends Maze(height, width) {
+  import scala.util.Random
+
+  private def wall = if (Random.nextInt(10) > 3) 1 else 0
+
+  (0 to height).foreach { r =>
+    (0 to width).foreach { c =>
+    }
+  }
+  cells(height)(width) = (1, 1)
 }
 
 object MazeGenerator {
