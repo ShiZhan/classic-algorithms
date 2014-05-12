724464987d4881ddcf9153279a8a93990d786f1f
ShiZhan <g.shizhan.g@gmail.com>
2014-05-12 04:18:31 -0500

    connected Maze

--- src/main/scala/Maze.scala
+++ src/main/scala/Maze.scala
@@ -1,10 +1,10 @@
 class Maze(height: Int, width: Int) {
-  val cells = Array.fill(height + 1, width + 1)((1, 1))
+  val vMat = Array.fill(height + 1, width + 1)(1)
+  val hMat = Array.fill(height + 1, width + 1)(1)
 
-  override def toString = cells.map { row =>
-    val (v, h) = row.unzip
-    val vLine = v.map { case 1 => "|"; case _ => " " }.mkString("  ", " ", "\n")
-    val hLine = h.map { case 1 => "-"; case _ => " " }.mkString(" ", "+", "+\n")
+  override def toString = (0 to height).map { r =>
+    val vLine = vMat(r).map { case 1 => "|"; case _ => " " }.mkString("  ", " ", "\n")
+    val hLine = hMat(r).map { case 1 => "-"; case _ => " " }.mkString(" ", "+", "+\n")
     vLine + hLine
   }.mkString
 
@@ -13,12 +13,11 @@ class Maze(height: Int, width: Int) {
     val g = Array.fill(cellTotal, cellTotal)(0)
     for (i <- (0 to (height - 1)))
       for (j <- (0 to (width - 1))) {
-        val (v, h) = cells(i + 1)(j + 1)
         val thisCell = i * width + j
         val southCell = (i + 1) % height * width + j
         val eastCell = i * width + ((j + 1) % width)
-        if (v == 0) { g(thisCell)(eastCell) = 1; g(eastCell)(thisCell) = 1 }
-        if (h == 0) { g(thisCell)(southCell) = 1; g(southCell)(thisCell) = 1 }
+        if (vMat(i + 1)(j + 1) == 0) { g(thisCell)(eastCell) = 1; g(eastCell)(thisCell) = 1 }
+        if (hMat(i + 1)(j + 1) == 0) { g(thisCell)(southCell) = 1; g(southCell)(thisCell) = 1 }
       }
     g
   }
@@ -31,9 +30,8 @@ case class SimpleMaze(height: Int, width: Int) extends Maze(height, width) {
 
   (0 to height).foreach { r =>
     (0 to width).foreach { c =>
-      val v = if (r == 0) 0 else if (c == 0 || c == width) 1 else wall
-      val h = if (c == 0) 0 else if (r == 0 || r == height) 1 else wall
-      cells(r)(c) = (v, h)
+      vMat(r)(c) = if (r == 0) 0 else if (c == 0 || c == width) 1 else wall
+      hMat(r)(c) = if (c == 0) 0 else if (r == 0 || r == height) 1 else wall
     }
   }
 }
@@ -41,15 +39,37 @@ case class SimpleMaze(height: Int, width: Int) extends Maze(height, width) {
 case class ConnectedMaze(height: Int, width: Int) extends Maze(height, width) {
   import scala.util.Random
 
-  cells(0)(0) = (0, 0)
-  (1 to height).foreach { cells(_)(0) = (1, 0) }
-  (1 to width).foreach { cells(0)(_) = (0, 1) }
+  val visited = Array.fill(height + 1, width + 1)(false)
+  private def isVisited(p: (Int, Int)) = visited(p._1)(p._2)
+
+  (0 to height).foreach { hMat(_)(0) = 0 }
+  (0 to width).foreach { vMat(0)(_) = 0 }
 
   implicit class Position(p: (Int, Int)) {
     val (r, c) = p
-    private def dig(to: Int) = {
+    val east = (r, c + 1)
+    val south = (r + 1, c)
+    val west = (r, c - 1)
+    val north = (r - 1, c)
+    def to(direction: Int) = direction match {
+      case 0 => if (c == width) (0, 0) else if (isVisited(east)) (0, 0) else { vMat(r)(c) = 0; east }
+      case 1 => if (r == height) (0, 0) else if (isVisited(south)) (0, 0) else { hMat(r)(c) = 0; south }
+      case 2 => if (c == 1) (0, 0) else if (isVisited(west)) (0, 0) else { vMat(r)(c - 1) = 0; west }
+      case 3 => if (r == 1) (0, 0) else if (isVisited(north)) (0, 0) else { hMat(r - 1)(c) = 0; north }
+      case _ => (0, 0)
     }
   }
+
+  private def dig(r: Int, c: Int): Unit = {
+    visited(r)(c) = true; print("*")
+    val anyDirection = Random.nextInt(4)
+    (0 to 3).map { d => (d + anyDirection) % 4 } foreach { d =>
+      val next = (r, c).to(d)
+      if (next != (0, 0)) dig(next._1, next._2)
+    }
+  }
+
+  dig(1, 1)
 }
 
 object SimpleMazeGenerator {
@@ -57,7 +77,8 @@ object SimpleMazeGenerator {
     val m = SimpleMaze(args(0).toInt, args(1).toInt)
     println(m)
     val g = Graph(m.toGraph)
-    println(g); println(g.reachable(0, args(0).toInt * args(1).toInt - 1))
+    println(g)
+    println(g.reachable(0, args(0).toInt * args(1).toInt - 1))
   }
 }
 
