5f51ef2ca2f6378882ca3af8373ad20538b3701e
ShiZhan <g.shizhan.g@gmail.com>
2014-05-14 17:50:53 -0500

    Seq[T]

--- src/main/scala/Entropy.scala
+++ src/main/scala/Entropy.scala
@@ -2,17 +2,14 @@ object Entropy {
   import scala.math._
   import scala.util.Random
 
-  def entropy(v: String) = {
+  def entropy[T](v: Seq[T]) = {
     v.groupBy(a => a).values
       .map(i => i.length.toDouble / v.length)
       .map(p => -p * log10(p) / log10(2))
       .sum
   }
 
-  def randomStr(length: Int) = {
-    val chars = ('a' to 'z') ++ ('A' to 'Z') ++ Array('\n', ' ')
-    Iterator.continually(chars(Random.nextInt(chars.length))).take(length)
-  }
+  def randomStr(length: Int) = Iterator.continually(Random.nextPrintableChar).take(length)
 
   def main(args: Array[String]) = {
     val information = if (args.length > 0) args.mkString(" ") else randomStr(100).mkString
