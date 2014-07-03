42dbe83c829f84ea6b0df394b9656ec20fc50b1b
ShiZhan <g.shizhan.g@gmail.com>
2014-07-02 21:59:52 -0500

    use actor to simulate Pregel vertex model

--- src/main/scala/BSP.scala
+++ src/main/scala/BSP.scala
@@ -2,8 +2,38 @@ class BspGraph(m: Array[Array[Int]]) extends Graph(m) {
   import scala.actors.Actor
   import scala.actors.Actor._
 
+  def in(index: Int) = m.map(_(index)).zipWithIndex.filter(_._1 != 0)
+  def out(index: Int) = m(index).zipWithIndex.filter(_._1 != 0)
+
+  sealed abstract class Messages
+
+  case class UPDATE(value: Int) extends Messages
+
+  case class STOP extends Messages
+
+  class Vertex(index: Int) extends Actor {
+    var value = Int.MaxValue
+    def act() = {
+      loop {
+        react {
+          case UPDATE(newValue) if (newValue < value) =>
+            value = newValue
+            for ((length, next) <- in(index)) {
+              println(s"$index->$next:$value+$length")
+              vertices(next) ! UPDATE(value + length)
+            }
+          case STOP => exit
+        }
+      }
+    }
+  }
+
+  val vertices = Array.tabulate(vertexTotal)(new Vertex(_))
+
   def shortestPath(target: Int) = {
-    println("TODO ...")
+    vertices.foreach(_.start)
+    vertices(target) ! UPDATE(0)
+    for (line <- io.Source.stdin.getLines) line match { case "exit" => vertices.foreach(_ ! STOP) }
   }
 }
 
@@ -11,9 +41,10 @@ object BspShortestPath {
   import scala.util.Random
 
   def main(args: Array[String]) = {
-    val vertexTotal = args(0).toInt
+    val vertexTotal = args.head.toInt
     val m = Array.fill(vertexTotal, vertexTotal)(Random.nextInt(2))
-    val g = new Graph(m)
+    val g = new BspGraph(m)
     println(g)
+    g.shortestPath(vertexTotal - 1)
   }
 }
