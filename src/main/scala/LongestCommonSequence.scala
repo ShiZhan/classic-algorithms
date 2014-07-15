5ebe39a4bd2dff72ddcba57af75d4f13d2dac53a
ShiZhan <g.shizhan.g@gmail.com>
2014-07-14 21:05:09 -0500

    type parameter

--- src/main/scala/LongestCommonSequence.scala
+++ src/main/scala/LongestCommonSequence.scala
@@ -1,31 +1,34 @@
 object LongestCommonSequence {
-  implicit class LCSop[T](left: Seq[T]) {
-    private def get(t: Array[Array[Int]], i: Int, j: Int) = if (i < 0 || j < 0) 0 else t(i)(j)
+  private def get(t: Array[Array[Int]], i: Int, j: Int) = if (i < 0 || j < 0) 0 else t(i)(j)
 
-    def <=>(right: Seq[T]) = {
-      val t = Array.fill(left.length, right.length)(0)
-      for ((l, i) <- left.zipWithIndex)
-        for ((r, j) <- right.zipWithIndex)
-          t(i)(j) = if (l == r) get(t, i - 1, j - 1) + 1 else get(t, i - 1, j) max get(t, i, j - 1)
+  type EqualOp[T] = (T, T) => Boolean
+  def LCS[T](left: Seq[T], right: Seq[T])(e: EqualOp[T]) = {
+    val t = Array.fill(left.length, right.length)(0)
+    for ((l, i) <- left.zipWithIndex; (r, j) <- right.zipWithIndex)
+      t(i)(j) = if (e(l, r)) get(t, i - 1, j - 1) + 1 else get(t, i - 1, j) max get(t, i, j - 1)
 
-      println(t.map(_.mkString(" ")).mkString("\n"))
-      val lcsLen = t(left.length - 1)(right.length - 1)
-      val lcs = Array.fill(lcsLen)((0, 0))
-      var (i, j) = (left.length - 1, right.length - 1)
-      while (i >= 0 && j >= 0) {
-        if (left(i) == right(j)) {
-          lcs(t(i)(j) - 1) = (i, j)
-          i -= 1
-          j -= 1
-        } else if (get(t, i - 1, j) > get(t, i, j - 1)) i -= 1 else j -= 1
-      }
-      lcs
+    t.map(_.mkString(" ")).foreach(println)
+
+    val lcsLen = t(left.length - 1)(right.length - 1)
+    val lcs = Array.fill(lcsLen)((0, 0))
+    var (i, j) = (left.length - 1, right.length - 1)
+    while (i >= 0 && j >= 0) {
+      if (e(left(i), right(j))) {
+        lcs(t(i)(j) - 1) = (i, j)
+        i -= 1
+        j -= 1
+      } else if (get(t, i - 1, j) > get(t, i, j - 1)) i -= 1 else j -= 1
     }
+    lcs
+  }
+
+  implicit class LCSop[T](left: Seq[T]) {
+    def <=>(right: Seq[T])(e: EqualOp[T]) = LCS[T](left, right)(e)
   }
 
   def main(args: Array[String]) = {
     val seq1 = args(0).toSeq
     val seq2 = args(1).toSeq
-    (seq1 <=> seq2).foreach(println)
+    (seq1 <=> seq2) { _ == _ }.foreach(println)
   }
 }
