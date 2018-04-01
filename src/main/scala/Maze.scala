d08cd7c773dbdf8c74ccf4229e033a3a35539150
Zhan.Shi <g.shizhan.g@gmail.com>
2018-04-01 15:35:14 +0800

    stack version

--- src/main/scala/Maze.scala
+++ src/main/scala/Maze.scala
@@ -85,7 +85,39 @@ class ConnectedMaze(height: Int, width: Int) extends Maze(height, width) {
     }
   }
 
-  dig(height / 2, width / 2)
+  private def dig_stack(r0: Int, c0: Int): Unit = {
+    import scala.collection.mutable.Stack
+    import scala.util.Random
+    var travel = Stack[(Int, Int, Int)]()
+    val toward = Random.nextInt(4)
+    visited(r0)(c0) = true
+    Random.shuffle(0 to 3).foreach { d => travel.push((r0, c0, d)) }
+
+    while(!travel.isEmpty) {
+      val (r, c, d) = travel.pop
+      d match {
+        case 0 if (c < width)  && !visited(r)(c+1) =>
+          vMat(r)(c) = 0
+          visited(r)(c + 1) = true
+          Random.shuffle(Seq(0,1,3)).foreach { d => travel.push((r, c + 1, d)) }
+        case 1 if (r < height) && !visited(r+1)(c) =>
+          hMat(r)(c) = 0
+          visited(r + 1)(c) = true
+          Random.shuffle(Seq(0,1,2)).foreach { d => travel.push((r + 1, c, d)) }
+        case 2 if (c > 1)      && !visited(r)(c-1) =>
+          vMat(r)(c-1) = 0
+          visited(r)(c - 1) = true
+          Random.shuffle(Seq(1,2,3)).foreach { d => travel.push((r, c - 1, d)) }
+        case 3 if (r > 1)      && !visited(r-1)(c) =>
+          hMat(r-1)(c) = 0
+          visited(r - 1)(c) = true
+          Random.shuffle(Seq(0,2,3)).foreach { d => travel.push((r - 1, c, d)) }
+        case _ =>
+      }
+    }
+  }
+
+  dig_stack(height / 2, width / 2)
 }
 
 object SimpleMazeGenerator {
